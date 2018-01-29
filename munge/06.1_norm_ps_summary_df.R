6b8520de3ef0c0fe249eb1bee54e302a880e5996
Nate Olson <nolson@nist.gov>
2018-01-29 13:13:39 -0500

    updated rds path

--- munge/06.1_norm_ps_summary_df.R
+++ munge/06.1_norm_ps_summary_df.R
@@ -1,6 +1,6 @@
 ## Rarified Count data summary 
 ProjectTemplate::cache("norm_summary_df", {
-    tibble(rds_file = list.files("munge/norm_data", full.names = TRUE)) %>% 
+    tibble(rds_file = list.files("data/norm_data", full.names = TRUE)) %>% 
         mutate(norm_ps = map(rds_file, readRDS),
                n_taxa = map_dbl(norm_ps, ntaxa),
                n_samples = map_dbl(norm_ps, nsamples)) %>% 
