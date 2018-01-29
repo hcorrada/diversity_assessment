cb4989088d2f23b6b2b1e5c4b746ac6722b7b135
Nate Olson <nolson@nist.gov>
2018-01-29 13:14:57 -0500

    converted to function - limit memory project memory footprint

---
+++ lib/make_beta_diversity_df.R
@@ -0,0 +1,26 @@
+## Generating Data Frame with weighted and unweighted diversity metrics 
+
+#' Make Tidy Beta Diversity Data Frame
+#'
+#' @param dist_methods character vector with diversity metrics to include in the data frame
+#' @param div_rds_path path to RDS files with dist objects for diversity metrics 
+#'
+#' @return data frame with a list column of diversity metrics
+#' @export
+#'
+#' @examples
+make_beta_div_df <- function(dist_methods, div_rds_path = "data/diversity_data"){
+    diversity_rds <- list.files(div_rds_path, full.names = TRUE)
+    
+    diversity_names <- basename(diversity_rds) %>% str_replace(".rds","") 
+    
+    diversity_df <- data_frame(div_info = diversity_names, 
+                               rds_file = diversity_rds) %>% 
+        separate(div_info, c("pipe","method","dist_method")) %>% 
+        mutate(dist_method = paste0(dist_method, "_dist"))
+    
+    diversity_df %>%
+            filter(dist_method %in% dist_methods) %>%
+            mutate(dist_results = map(rds_file, readRDS)) %>% 
+            select(-rds_file)
+}
--- munge/07.1_cache_beta_diversity_df.R
+++ munge/07.1_cache_beta_diversity_df.R
@@ -1,27 +1,29 @@
-## Generating Data Frame with weighted and unweighted diversity metrics 
-diversity_rds <- list.file("munge/diversity_data", full.names = TRUE)
-
-diversity_names <- basename(diversity_rds) 
-
-diversity_df <- data_frame(div_info, rds_file = diversity_rds) %>% 
-    separate(div_info, c("pipe","method","dist_method")) %>% 
-    mutate(dist_method = paste0(dist_method, "_dist")) %>% 
-    select(-div_info) 
-
-############## Unweighted metrics - rarified data only #########################
-## Unweighted metrics include unweighted unifrac and jaccard
-ProjectTemplate::cache("unweighted_beta_df", {
-    diversity_df %>%
-        filter(dist_method %in% c("unifrac", "jaccard")) %>%
-        mutate(dist_results = map(rds_file, readRDS)) %>% 
-        select(-rds_file)
-})
-
-########### Weighted Metrics ###################################################
-## Weighted metrics evaluated compared include weighted unifrac and bray-curtis
-ProjectTemplate::cache("weighted_beta_df", {
-    diversity_df %>%
-        filter(dist_method %in% c("wunifrac", "bray")) %>%
-        mutate(dist_results = map(rds_file, readRDS)) %>% 
-        select(-rds_file)
-})
+# ## Generating Data Frame with weighted and unweighted diversity metrics
+# diversity_rds <-
+#     list.files("data/diversity_data", full.names = TRUE)
+# 
+# diversity_names <-
+#     basename(diversity_rds) %>% str_replace(".rds", "")
+# 
+# diversity_df <- data_frame(div_info = diversity_names,
+#                            rds_file = diversity_rds) %>%
+#     separate(div_info, c("pipe", "method", "dist_method")) %>%
+#     mutate(dist_method = paste0(dist_method, "_dist"))
+# 
+# ############## Unweighted metrics - rarified data only #########################
+# ## Unweighted metrics include unweighted unifrac and jaccard
+# ProjectTemplate::cache("unweighted_beta_df", {
+#     diversity_df %>%
+#         filter(dist_method %in% c("unifrac_dist", "jaccard_dist")) %>%
+#         mutate(dist_results = map(rds_file, readRDS)) %>%
+#         select(-rds_file)
+# })
+# 
+# ########### Weighted Metrics ###################################################
+# ## Weighted metrics evaluated compared include weighted unifrac and bray-curtis
+# ProjectTemplate::cache("weighted_beta_df", {
+#     diversity_df %>%
+#         filter(dist_method %in% c("wunifrac_dist", "bray_dist")) %>%
+#         mutate(dist_results = map(rds_file, readRDS)) %>%
+#         select(-rds_file)
+# })
