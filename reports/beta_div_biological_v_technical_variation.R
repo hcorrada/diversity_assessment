2336f759c1f4b7e50177a819e93b7d2b29b72b0b
Jacquelyn Meisel <jsmeisel1@gmail.com>
2018-02-06 16:21:14 -0500

    forgot updated plot code

--- reports/beta_div_biological_v_technical_variation.R
+++ reports/beta_div_biological_v_technical_variation.R
@@ -235,7 +235,7 @@ print(ggplot(subset(biol_v_tech_variation_ma_plot,
           xlab("Overall Mean Distance")+ ylab("Mean Biological Distance - Mean Technical Distance"))
 dev.off()
 
-pdf("./reports/biological_v_technical_variation_diversity_varpart_stats.pdf", height=16, width=24)
+pdf("./reports/biological_v_technical_variation_diversity_varpart_stats.pdf", height=14, width=16)
 
 varpart_stats$normalization<-factor(varpart_stats$normalization, levels=c("CSS", "TMM", "TSS", "RLE", "UQ", "RAW", "rare10000", 
                                                                           "rare5000", "rare2000", "rareq15"), ordered = T)
@@ -245,6 +245,16 @@ varpart_stats$txt_label<-paste0(round(varpart_stats$Adj.R.square, 2), "\n", varp
 print(ggplot(subset(varpart_stats, effect=="conditional"), aes(x=pipe, y=normalization, fill=Adj.R.square))+geom_tile()+facet_grid(metric~feature, scales="free", space="free")+
     theme_bw()+theme(axis.text.x=element_text(angle = -45, hjust = 0))+
     scale_fill_distiller(palette = "YlGn", direction = 1)+ geom_text(aes(label=txt_label))+ 
-        ggtitle("Biological vs. Technical Variation varpart Stats"))
+        ggtitle("Biological vs. Technical Variation varpart Stats: Conditional (partial) Variation"))
+
+print(ggplot(subset(varpart_stats, effect=="marginal"), aes(x=pipe, y=normalization, fill=Adj.R.square))+geom_tile()+facet_grid(metric~feature, scales="free", space="free")+
+          theme_bw()+theme(axis.text.x=element_text(angle = -45, hjust = 0))+
+          scale_fill_distiller(palette = "YlGn", direction = 1)+ geom_text(aes(label=txt_label))+ 
+          ggtitle("Biological vs. Technical Variation varpart Stats: Marginal (simple) Variation"))
+
+print(ggplot(subset(varpart_stats, effect=="global"), aes(x=pipe, y=normalization, fill=Adj.R.square))+geom_tile()+facet_grid(~metric, scales="free", space="free")+
+          theme_bw()+theme(axis.text.x=element_text(angle = -45, hjust = 0))+
+          scale_fill_distiller(palette = "YlGn", direction = 1)+ geom_text(aes(label=txt_label))+ 
+          ggtitle("Biological vs. Technical Variation varpart Stats: Global Variation"))
 
 dev.off()
