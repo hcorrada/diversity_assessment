a691fc5923c9546110eb2f1cd012db4e19174a7c
Jacquelyn Meisel <jsmeisel1@gmail.com>
2018-03-13 17:52:03 -0400

    calculating singletons and identifing number of reads/samples/taxa at each rarefaction level

--- munge/05_pipeline_characteristics.R
+++ munge/05_pipeline_characteristics.R
@@ -16,6 +16,20 @@ calc_sparsity <- function(ps){
     nzero/nentry
 }
 
+## Identify number of singletons in each dataset
+calc_singletons <- function(ps){ 
+    
+    ## Excluding no template controls
+    non_ntc_samples <- sample_data(ps)$biosample_id != "NTC"
+    ps <- prune_samples(non_ntc_samples, ps)
+    
+    ## calculate number of singletons
+    nsingle <- length(which(taxa_sums(ps)==1))
+    
+    ## singletons
+    nsingle
+}
+
 ## Pipeline characterization
 ## Columns 
 # - `pipe` - pipeline name  
@@ -35,6 +49,7 @@ get_pipe_info <- function(){
             pipe = str_extract(rds_file, "(?<=ts/).*(?=_ps)"),
             ps = map(rds_file, readRDS),
             n_taxa = map_dbl(ps, ntaxa),
+            num_singletons = map_dbl(ps, calc_singletons),
             n_samples = map_dbl(ps, nsamples), 
             sparsity = map_dbl(ps, calc_sparsity), 
             taxa_per_sample = map(ps, estimate_richness, measures = "Observed"),
@@ -52,9 +67,9 @@ get_pipe_info <- function(){
         mutate(sample_id = str_replace(sample_id, "\\.","-")) %>% 
         left_join(lib_size_df) %>% 
         mutate(pass_rate = total_abu / read) %>% 
-        select(pipe, n_taxa, n_samples, sparsity, sample_id, 
+        select(pipe, n_taxa, n_samples, sparsity, num_singletons, sample_id, 
                Observed, total_abu, read, pass_rate) %>% 
-        group_by(pipe, n_taxa, n_samples, sparsity) %>% 
+        group_by(pipe, n_taxa, n_samples, sparsity, num_singletons) %>% 
         nest() %>% 
         dplyr::rename(per_sample_info = data)
         
@@ -63,3 +78,37 @@ get_pipe_info <- function(){
 ProjectTemplate::cache(variable = "pipe_char_df", 
                        CODE = {get_pipe_info()}, 
                        depends = c("mgtstMetadata","seq_char_df"))
+
+
+## Identify number of reads per sample at each rarefaction level
+calc_rare_lev <- function(ps){ 
+    
+    ## calculate number of reads at rarefaction level
+    rare_lev <- unique(sample_sums(ps))
+    
+    rare_lev
+}
+
+## Rarefaction characterization
+## Columns 
+# - `pipe` - pipeline name 
+# - `normalization` - normalization method
+# - `n_taxa` - number of features in count table  
+# - `n_samples` - number of samples in count table  
+
+get_rare_info <- function(){
+    rds_file = list.files("data/norm_data", full.names = TRUE)
+    rare_info_df <- tibble(file=rds_file[grep("rare", rds_file)]) %>%
+        mutate(
+            pipe = gsub(file, pattern="(data/norm_data/)([a-z].*)_(rare[0-9q][0-9]*).rds", replacement="\\2"),
+            norm = gsub(file, pattern="(data/norm_data/)([a-z].*)_(rare[0-9q][0-9]*).rds", replacement="\\3"),
+            ps = map(file, readRDS),
+            n_reads = map_dbl(ps, calc_rare_lev),
+            n_taxa = map_dbl(ps, ntaxa),
+            n_samples = map_dbl(ps, nsamples)
+           ) %>%
+        select(-ps, -file) 
+}
+
+ProjectTemplate::cache(variable = "rare_char_df", 
+                       CODE = {get_rare_info()})
