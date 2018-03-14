6a09a0a992ed25e5487c3c6717b9f9bd70b1abed
Nate Olson <nolson@nist.gov>
2018-03-14 15:32:17 -0400

    added plot color vars and fixed formatting

---
+++ src/plot_color_definitions.R
@@ -0,0 +1,37 @@
+## Plot color themes
+run_colors <- c(
+    jhu_run1 = "#E41A1C", jhu1 = "#E41A1C", 
+    jhu_run2 = "#4A72A6", jhu2 = "#4A72A6",
+    nist_run1 = "#48A462", nist1 = "#48A462",
+    nist_run2 = "#A35390", nist2 = "#A35390"
+)
+
+run_colors2 <- c(
+    'jhu_run1_2-A10' = "#E41A1C",
+    'jhu_run2_2-A10' = "#4A72A6",
+    'nist_run1_2-A10' = "#48A462",
+    'nist_run2_2-A10' = "#A35390"
+)
+
+
+pipe_colors <- c(
+    dada = "#5D995D",
+    deblur = "#bf548c",
+    mothur = "#ef8a1e",
+    qiimeClosedRef = "#f2ae02",
+    qiimeOpenRef = "#c63f4a",
+    qiimeDeNovo = "#7190c4"
+)
+
+norm_colors <- c(
+    rare2000 = "#974661",
+    rare5000 = "#3E8E93",
+    rare10000 = "#9b6ba3",
+    RAW = "black",
+    rareq15 = "#FFA100",
+    RLE = "#DB728C",
+    CSS = "#B75F49",
+    TMM = "#526E9F",
+    UQ = "#f2d51a",
+    TSS = "#66ad85"
+)
